334fff9af52862c76920b194e1e1f800aa958ac0
LorenzoLatinnePXL <11701160@student.pxl.be>
2024-11-27 10:39:43 +0100

    Deel 1 en Deel 2.

---
+++ WPFArrayFuncties/WPFArrayFuncties/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFArrayFuncties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namen = { "Peter", "Tom", "Piet", "Ahmed", "Benny", "Anna", "Jane", "Ali", "John", "Tom" };
+
+            Console.Write("Geef een naam: ");
+            string naam = Console.ReadLine();
+
+            Console.WriteLine($"De naam {naam} staat op index: {Array.IndexOf(namen, naam)}.");
+
+            Console.Write("Geef een letter: ");
+            string letterVanNaam = Console.ReadLine().ToUpper();
+            string[] nameStartsWithLetter = Array.FindAll(namen, name => name.StartsWith(letterVanNaam));
+
+            Console.Write($"Namen die beginnen met {letterVanNaam}: ");
+            foreach ( string name in nameStartsWithLetter )
+            {
+                Console.Write(name + " ");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
